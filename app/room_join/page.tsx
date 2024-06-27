ee4817efc4ef87d9799e41353e6d17f1ab8764b4
masa-massara <124395897+masa-massara@users.noreply.github.com>
2024-06-27 23:33:17 +0900

    fix:join画面をDnDに変更

--- app/room_join/page.tsx
+++ app/room_join/page.tsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import InputTeampassword from '../ui/components/InputTeamPassword'
-import styles from './Page.module.css'
-import JoinRoomButton from '../ui/components/JoinRoomButton'
+import React from "react";
+import InputTeampassword from "../ui/components/InputTeamPassword";
+import styles from "./Page.module.css";
+import JoinRoomButton from "../ui/components/JoinRoomButton";
+import InputRoomPass from "../ui/components/InputRoomPass/InputRoomPass";
 
 const page = () => {
   return (
     <div className={styles.container}>
       <h1>部屋に参加</h1>
       <p>部屋のパスワードを入力してください</p>
-      <InputTeampassword />
+
+      <InputRoomPass />
       <JoinRoomButton />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
