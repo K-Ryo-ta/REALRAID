0e9496c37694a87645c91b9b442cd66b094406ca
yukiringo <141327996+yukiringo-apple@users.noreply.github.com>
2024-06-28 03:50:18 +0900

    join css

--- app/room_join/page.tsx
+++ app/room_join/page.tsx
@@ -6,7 +6,7 @@ import JoinRoomButton from '../ui/components/JoinRoomButton'
 const page = () => {
   return (
     <div className={styles.container}>
-      <div className='background_large'>
+      <div className={styles.background_large}>
         <h1 className={styles.h1}>部屋に参加</h1>
         <p className={styles.p}>部屋のパスワードを入力してください</p>
         <InputTeampassword />
