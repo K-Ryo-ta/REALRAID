32a23b20109cb1f91567a699bf390449ced57bc5
masa-massara <124395897+masa-massara@users.noreply.github.com>
2024-06-28 13:16:56 +0900

    fix:インポートを修正

--- app/room_join/page.tsx
+++ app/room_join/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import InputTeampassword from "../ui/components/InputTeamPassword";
 import styles from "./Page.module.css";
 import JoinRoomButton from "../ui/components/JoinRoomButton";
-import InputRoomPass from "../ui/components/InputRoomPass/InputRoomPass";
+import InputRoomPass from "../ui/components/inputRoomPass/InputRoomPass";
 
 const page = () => {
   return (
@@ -10,14 +10,11 @@ const page = () => {
       <div className={styles.background_large}>
         <h1 className={styles.h1}>部屋に参加</h1>
         <p className={styles.p}>部屋のパスワードを入力してください</p>
-        <InputTeampassword />
+        <InputRoomPass />
         <JoinRoomButton />
       </div>
-
     </div>
   );
 };
 
-
 export default page;
-
