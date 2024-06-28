8c63d1e61cfe1f389c42a18bd6e7cc6174fe0f21
masa-massara <124395897+masa-massara@users.noreply.github.com>
2024-06-29 06:00:38 +0900

    feat:registerのバリデーションを追加

--- app/ui/components/RegisterButton.tsx
+++ app/ui/components/RegisterButton.tsx
@@ -1,20 +1,39 @@
-'use client';
-import React, { useState } from 'react';
-import styles from '../../register/Page.module.css';
-import { useRouter } from 'next/navigation';
-import { useSaveUserinFirebase } from '@/app/lib/userhooks';
-import { useRecoilValue } from 'recoil';
-import { roomfullState } from '@/app/states';
+"use client";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useSaveUserinFirebase } from "@/app/lib/userhooks";
+import { useRecoilState, useRecoilValue } from "recoil";
+import {
+  canSubmitState,
+  roomfullState,
+  teamnameState,
+  usernameState,
+} from "@/app/states";
 
 const RegisterButton: React.FC = () => {
   const router = useRouter();
   const saveUser = useSaveUserinFirebase();
   const roomfull = useRecoilValue(roomfullState);
   const [isLoading, setIsLoading] = useState(false);
+  const [teamname, setTeamname] = useRecoilState<string>(teamnameState);
+  const [username, setUsername] = useRecoilState<string>(usernameState);
+  const [canSubmit, setCanSubmit] = useRecoilState<boolean>(canSubmitState);
 
-  const handleButtonClick = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  const handleButtonClick = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
     e.preventDefault();
-    console.log('ボタンが押されました');
+
+    // teamname と username が空でないかを確認
+    const canSubmitValue = Boolean(teamname && username);
+    setCanSubmit(canSubmitValue);
+    if (!canSubmitValue) {
+      console.log("名前とチーム名を入力してください");
+      return;
+    }
+
+    console.log("ボタンが押されました");
+
     setIsLoading(true); // ローディング状態を設定
 
     const isRoomFull = await saveUser();
@@ -22,22 +41,24 @@ const RegisterButton: React.FC = () => {
 
     // saveUser() の完了後に最新の roomfull の値を確認
     if (!isRoomFull) {
-      router.push('/room_create_or_join');
+      router.push("/room_create_or_join");
     } else {
-      console.log('このチームは満員です');
+      console.log("このチームは満員です");
     }
   };
 
   return (
     <div>
-      <button onClick={handleButtonClick} className='button_middle_background' disabled={isLoading}>
-        <div className='button_middle_front'>
-          登録
-        </div>
+      <button
+        onClick={handleButtonClick}
+        className="button_middle_background"
+        disabled={isLoading}
+      >
+        <div className="button_middle_front">登録</div>
       </button>
       {isLoading && <p>処理中...</p>}
+      {!canSubmit && <p>チーム名と名前は必須です。</p>}
     </div>
   );
 };
-
 export default RegisterButton;
