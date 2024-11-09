c46134ced2c40cc5244687ab6f6bf0d0d0653a57
K-Ryo-ta <studyprograming51@gmail.com>
2024-11-10 00:01:49 +0900

    バグ直せた。多分

--- app/check_theme_and_answer/page.tsx
+++ app/check_theme_and_answer/page.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import GameStartButton from "../ui/components/GameStartButton";
 import CorrectList from "../ui/components/CorrectList";
+import styles from "./Page.module.css";
 
 const page = () => {
   return (
-    <div>
-      <h1>正解リストの一部を紹介！！</h1>
-      <CorrectList />
-      <GameStartButton />
+    <div className={styles.container}>
+      <div className={styles.background}>
+        <h1 className={styles.h1}>正解の一部を紹介！！</h1>
+        <CorrectList />
+        <GameStartButton />
+      </div>
     </div>
   );
 };
