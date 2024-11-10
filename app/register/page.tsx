67c3310132f383814bf583d1f3200cd2c7a29f89
masa-massara <124395897+masa-massara@users.noreply.github.com>
2024-11-10 10:27:24 +0900

    insta,twitterのidを取得

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -3,28 +3,32 @@ import styles from "./Page.module.css";
 import InputTeamname from "../ui/components/Formcomponents/InputTeamname";
 import InputUsername from "../ui/components/Formcomponents/InputUsername";
 import RegisterButton from "../ui/components/Formcomponents/RegisterButton";
+import InputInstagramId from "../ui/components/Formcomponents/InputInstagramId";
+import InputTwitterId from "../ui/components/Formcomponents/InputTwitterId";
 
 const page = () => {
-  return (
-    <div className={styles.container}>
-      <div className="background_large">
-        <h1 className={styles.anm}>チーム名とおなまえ 登録</h1>
-        <p className={styles.font_small}>
-          ◾️◾️◾️ チーム名 ◾️◾️◾️<br></br>
-          ランキングで表示されるよ！<br></br>
-          一緒にプレイする友達と<br></br>
-          同じチーム名を入力してね！
-        </p>
-        <InputTeamname />
-        <p className={styles.font_small}>
-          ◾️◾️◾️ なまえ ◾️◾️◾️<br></br>
-          フルネームをひらがなで入力してね！
-        </p>
-        <InputUsername />
-        <RegisterButton />
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+			<div className="background_large">
+				<h1 className={styles.anm}>チーム名とおなまえ 登録</h1>
+				<p className={styles.font_small}>
+					◾️◾️◾️ チーム名 ◾️◾️◾️<br></br>
+					ランキングで表示されるよ！<br></br>
+					一緒にプレイする友達と<br></br>
+					同じチーム名を入力してね！
+				</p>
+				<InputTeamname />
+				<p className={styles.font_small}>
+					◾️◾️◾️ なまえ ◾️◾️◾️<br></br>
+					フルネームをひらがなで入力してね！
+				</p>
+				<InputUsername />
+				<InputInstagramId />
+				<InputTwitterId />
+				<RegisterButton />
+			</div>
+		</div>
+	);
 };
 
 export default page;
